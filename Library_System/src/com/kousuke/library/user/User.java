e95c93e790299279c5f90f87de7b22ea87094d62
noboji2525 <k.hayakawa@crane-i.co.jp>
2020-12-10 00:53:46 +0900

    2020/12/10登録番号を追加

--- Library_System/src/com/kousuke/library/user/User.java
+++ Library_System/src/com/kousuke/library/user/User.java
@@ -1,25 +1,34 @@
 package com.kousuke.library.user;
 
-public class User extends Human{
+public class User implements Human{
+	public static int count=0;
 	private int number;
+	private String name;
+
 
 	public User() {
 
 	}
 	public User(String name) {
-		super(name);
-
+		this.name = name;
+		count++;
+		this.number = count;
 	}
 
 
 	//本を借りる
+	@Override
 	public void bookRental() {
 
 	}
 	//本を返却
+	@Override
 	public void returnBook() {
 
 	}
+
+	public String getName() {return name;}
+	public void setName(String name) {this.name = name;}
 
 	public int getNumber() {return number;}
 	public void setNumber(int number) {this.number = number;}
