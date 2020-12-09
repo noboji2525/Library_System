546a43f2e1412c1852594fba9f43981e46ce1074
noboji2525 <k.hayakawa@crane-i.co.jp>
2020-12-10 00:54:26 +0900

    ArrayList<String>を<User>に修正

--- Library_System/src/com/kousuke/library/user/UserList.java
+++ Library_System/src/com/kousuke/library/user/UserList.java
@@ -4,20 +4,21 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class UserList {
-	User user = new User();
-	public List<String> userList = new ArrayList<String>();
+//	User user = new User();
+	public List<User> userList = new ArrayList<User>();
 	public UserList() {
 
 	}
 	//利用者登録
-	public void userRegistration(String name) {
-		user.setName(name);
-		userList.add(user.getName());
+	public void userRegistration(User user) {
+		userList.add(user);
 	}
 	//利用者削除
 	public void userDelet(int listNumber) {
 		userList.remove(listNumber);
 	}
+
+
 
 
 }
