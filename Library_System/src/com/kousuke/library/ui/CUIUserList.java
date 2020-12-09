f14b9d0c79dd7038c98a95f25e7004bac692d131
noboji2525 <k.hayakawa@crane-i.co.jp>
2020-12-10 00:55:07 +0900

    2020/12/10 ArrayList<String>→<User>に修正

--- Library_System/src/com/kousuke/library/ui/CUIUserList.java
+++ Library_System/src/com/kousuke/library/ui/CUIUserList.java
@@ -1,5 +1,6 @@
 package com.kousuke.library.ui;
 
+import com.kousuke.library.user.User;
 
 public class CUIUserList {
 
@@ -19,20 +20,22 @@ public class CUIUserList {
 					System.out.println("利用者の登録をします");
 					System.out.print("名前を入力してください：");
 					String name = CUIMainMenu.sc.next();
-					CUIMainMenu.userList.userRegistration(name);
+					User user = new User(name);
+					CUIMainMenu.userList.userRegistration(user);
 					System.out.println(String.format("『%s』の登録を致しました",name));
+					System.out.println(String.format("登録番号は【%1$03d】です",user.getNumber()));
 					System.out.println("---");
 					break;
 				case 2://利用者削除
 					System.out.println("登録済みの利用者を削除します");
 					int size = CUIMainMenu.userList.userList.size();//userListの配列の長さ
 					for(int i=0; i < size; i++) {
-						System.out.println(i+1 + ". " + CUIMainMenu.userList.userList.get(i));
+						System.out.println(i+1 + ". " + CUIMainMenu.userList.userList.get(i).getName());
 					}
 					System.out.println("削除したい利用者の番号を入力してください");
 					int number = CUIMainMenu.sc.nextInt();
 					int listNumber = number -1;
-					System.out.println(String.format("『%s』を削除しました",CUIMainMenu.userList.userList.get(listNumber)));
+					System.out.println(String.format("『%s』を削除しました",CUIMainMenu.userList.userList.get(listNumber).getName()));
 					CUIMainMenu.userList.userDelet(listNumber);
 					System.out.println("---");
 					break;
